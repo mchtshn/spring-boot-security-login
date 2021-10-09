08c5854dd65a78af4fe15aa38fbfb09473ad7cae
mchtshn <mucahit.shn7@gmail.com>
2021-10-09 17:08:16 +0300

    main applicaiton moved from system to outside

--- src/main/java/com/mucahit/springsecuritylogin/SpringSecurityLoginApplication.java
+++ src/main/java/com/mucahit/springsecuritylogin/SpringSecurityLoginApplication.java
@@ -1,4 +1,4 @@
-package com.mucahit.springsecuritylogin.system;
+package com.mucahit.springsecuritylogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
