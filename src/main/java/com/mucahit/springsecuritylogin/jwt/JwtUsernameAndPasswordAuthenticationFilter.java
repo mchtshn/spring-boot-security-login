d69e88d23924b7eb0f4cecd082f99c7a8fb23823
mchtshn <mucahit.shn7@gmail.com>
2021-10-10 12:15:20 +0300

    jwt token cretaed

--- src/main/java/com/mucahit/springsecuritylogin/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/mucahit/springsecuritylogin/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,17 +1,26 @@
 package com.mucahit.springsecuritylogin.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.Date;
 
 //job of this class is to verify the credintials.spring security does it default.we are going to override and implement it
+
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
@@ -21,6 +30,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
     }
 
 
+    //client sends credential ,server validates credentials.
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         //grab username and password
@@ -32,5 +42,20 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
             throw new RuntimeException(e);
         }
         return super.attemptAuthentication(request, response);
+    }
+
+    //this method will be invoked after  the attemptAuthentication is succesfull. if the authentication method fails ,this will be never executed.
+    //this methods creates token
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String key = "securesecuresecuresecuresecuresecuresecuresecuresecuresecure";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
